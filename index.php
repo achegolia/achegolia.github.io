0321650ec3ac79820087aac8c8a17ec792cfe87d
achegolia <achegolia@yahoo.com>
2023-02-14 06:42:14 +0000

    func getEmail func getEmail

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
 <?php 					
-	if( isset($_POST['email']) )
-	{
-		$email = $_POST['email'];
-	} else {
-		$email = "no email";
+	function getEmail (){
+
+		if( isset($_POST['email']) )
+		{
+			$email = $_POST['email'];
+		} else {
+			$email = "no email";
+		}
+		
+		return $email;
 	}
 	
-	return $email;
+
 ?>
